ebd592063e0273b2bb058f4bc25d8231bec77f7a
itsamirrezah <itsamirrezah@gmail.com>
2019-12-11 18:44:24 +0330

    Add Colors & Dimens

--- gradlew.bat
+++ gradlew.bat
@@ -52,13 +52,13 @@ if not "%OS%" == "Windows_NT" goto win9xME_args
 
 :win9xME_args
 @rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
+set CLINE_ARGS=
 set _SKIP=2
 
 :win9xME_args_slurp
 if "x%~1" == "x" goto execute
 
-set CMD_LINE_ARGS=%*
+set CLINE_ARGS=%*
 
 :execute
 @rem Setup the command line
@@ -66,7 +66,7 @@ set CMD_LINE_ARGS=%*
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CLINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
