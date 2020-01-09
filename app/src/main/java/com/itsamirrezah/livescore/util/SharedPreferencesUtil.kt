3ce3efa2649091b6f1029c4caef79deaec904215
itsamirrezah <itsamirrezah@gmail.com>
2020-01-09 17:13:29 +0330

    Make changes in shared preference -add two new variable: --serverCompetitions: list of competitions that api allows to use --localCompetitions: list of competitions that we already put their teams info on database

--- app/src/main/java/com/itsamirrezah/livescore/util/SharedPreferencesUtil.kt
+++ app/src/main/java/com/itsamirrezah/livescore/util/SharedPreferencesUtil.kt
@@ -3,6 +3,9 @@ package com.itsamirrezah.livescore.util
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.itsamirrezah.livescore.ui.model.CompetitionUi
 
 class SharedPreferencesUtil(context: Context) {
 
@@ -12,13 +15,28 @@ class SharedPreferencesUtil(context: Context) {
         pref = PreferenceManager.getDefaultSharedPreferences(context)
     }
 
-    var statusSelected: String
-        get() = pref!!.getString("STATUS_SELECTED", "") as String
+    var statusSelected: String?
+        get() = pref!!.getString("STATUS_SELECTED", "")
         set(value) = pref!!.edit().putString("STATUS_SELECTED", value).apply()
 
-    var competitionsSelected: Set<String>
-        get() = pref!!.getStringSet("COMPETITIONS_SELECTED", setOf()) as Set<String>
+    var competitionsSelected: Set<String>?
+        get() = pref!!.getStringSet("COMPETITIONS_SELECTED", setOf())
         set(value) = pref!!.edit().putStringSet("COMPETITIONS_SELECTED", value).apply()
+
+    var serverCompetitions: List<CompetitionUi>
+        get() = mapGsonToCompUiModel(pref!!.getString("SERVER_COMPETITION", null))
+        set(value) = pref!!.edit().putString("SERVER_COMPETITION", Gson().toJson(value)).apply()
+
+    var localCompetitions: Set<String>?
+        get() = pref!!.getStringSet("LOCAL_COMPETITION", setOf())
+        set(value) = pref!!.edit().putStringSet("LOCAL_COMPETITION", value).apply()
+
+    private fun mapGsonToCompUiModel(gson: String?): List<CompetitionUi> {
+        if (gson == null)
+            return listOf()
+        val compListType = object : TypeToken<List<CompetitionUi>>() {}.type
+        return Gson().fromJson(gson, compListType) as List<CompetitionUi>
+    }
 
     companion object {
         var instance: SharedPreferencesUtil? = null
